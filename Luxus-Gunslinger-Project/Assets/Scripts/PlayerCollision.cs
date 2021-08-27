81c5ec3b309649a6617537500d23a6a3ff4dc839
MDenzel031 <mdenzel31ugaban@gmail.com>
2021-08-27 20:44:39 +0800

    Fixing Parallax Backgrounds

--- Luxus-Gunslinger-Project/Assets/Scripts/PlayerCollision.cs
+++ Luxus-Gunslinger-Project/Assets/Scripts/PlayerCollision.cs
@@ -7,15 +7,44 @@ public class PlayerCollision : MonoBehaviour
 
     public Animator playerAnimation;
 
+
+    private Sound source;
+    bool isPlaying = false;
+
+    private void Start()
+    {
+        source = FindObjectOfType<AudioManager>().getSound("deathSound");
+    }
+
+    private void Update()
+    {
+        if(isPlaying == true)
+        {
+            if (!source.source.isPlaying)
+            {
+
+                gameOver();
+                
+            }
+        }
+    }
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.collider.tag == "DeathObstacle/Spike" || collision.collider.tag == "DeathObstacle/Water")
         {
-            disablePlayerMovement();
-            playerAnimation.SetBool("isHurt", true);
-            Invoke("destroyObject", 1f);
-            gameOver();
+            playerDeath();
         }
+    }
+
+    public void playerDeath()
+    {
+        disablePlayerMovement();
+        playerAnimation.SetBool("isHurt", true);
+        FindObjectOfType<AudioManager>().stopSound("bgMusic");
+        source.source.Play();
+        isPlaying = true;
+
     }
 
 
--- Luxus-Gunslinger-Project/Assets/Weapon.cs
+++ Luxus-Gunslinger-Project/Assets/Weapon.cs
@@ -7,19 +7,29 @@ public class Weapon : MonoBehaviour
 
     public Transform firePoint;
     public GameObject bulletPrefab;
+    public Animator weaponAnimation;
+    bool fireAnimation = false;
 
 
     void Update()
     {
         if (Input.GetButtonDown("Fire1"))
         {
+            fireAnimation = !fireAnimation;
+            weaponAnimation.SetBool("isFiring", fireAnimation);
             Shoot();
         }
+
     }
 
     void Shoot()
     {
+        
         Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
         FindObjectOfType<AudioManager>().playSound("gunShot");
+
+
+
+
     }
 }
